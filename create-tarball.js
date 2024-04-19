99510f69f718e2a5bc85312ed448842133ed6efe
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-04-19 16:30:28 -0700

    Attach directly without external Action

--- create-tarball.js
+++ create-tarball.js
@@ -1,12 +1,20 @@
-module.exports = async ({ context, core, exec }) => {
-  // Ensure that the GITHUB_REF is a tag
-  if (context.ref.startsWith('refs/tags/v')) {
-    const tag = context.ref.replace('refs/tags/v', '')
-    const tarball = `${context.repo.repo}-with-submodules-${tag}.tar.gz`
-
-    await exec.exec('tar', ['--exclude-vcs', '-czvf', tarball, context.repo.repo])
-    core.setOutput('tar-file-path', tarball)
-  } else {
-    core.setFailed('GITHUB_REF is not a tag!')
+module.exports = async ({ context, core, exec, process, github, fs }) => {
+  // Make sure that there is an existing release
+  if (context.eventName !== 'release' || context.payload.action !== 'published') {
+    core.setFailed('Action must be triggered by a `release.published` event!')
+    process.exit()
   }
+
+  const filename = `${context.repo.repo}-with-submodules-${context.payload.release.tag_name}.tar.gz`
+  const tarball = `${process.env.RUNNER_TEMP}/${filename}`
+
+  await exec.exec('tar', ['--exclude-vcs', '-czvf', tarball, context.repo.repo])
+
+  await github.rest.repos.uploadReleaseAsset({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    release_id: context.payload.release.id,
+    name: filename,
+    data: await fs.readFileSync(tarball)
+  })
 }
