15fd1046854c126fd9d9ab5ab05a214cd746f809
Nicholas Wiltsie <nwiltsie@mednet.ucla.edu>
2024-04-22 09:16:40 -0700

    Add more comments

--- create-tarball.js
+++ create-tarball.js
@@ -8,8 +8,10 @@ module.exports = async ({ context, core, exec, process, github, fs }) => {
   const filename = `${context.repo.repo}-with-submodules-${context.payload.release.tag_name}.tar.gz`
   const tarball = `${process.env.RUNNER_TEMP}/${filename}`
 
+  // Create the tarball, excluding any .git folders
   await exec.exec('tar', ['--exclude-vcs', '-czvf', tarball, context.repo.repo])
 
+  // Upload the tarball to the release
   await github.rest.repos.uploadReleaseAsset({
     owner: context.repo.owner,
     repo: context.repo.repo,
